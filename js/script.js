0a77e5076d63f88b7c802391944ba0f275fc552a
João Lustosa <joaovitorllds@gmail.com>
2023-06-28 01:44:55 -0300

    feat: implement measurement system update based on selection

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // Function to change the measurement system (Metric or Imperial)
 function changeMeasurementSystem() {
+    // Get the selected measurement system
+    const measurementSystem = document.getElementById('measurement-system').value;
+
+    // Get the weight and height unit elements
+    const weightUnit = document.getElementById('weight-unit');
+    const heightUnit = document.getElementById('height-unit');
+
+    // Update weight and height units based on the selected measurement system
+    if (measurementSystem === 'metric') {
+        weightUnit.innerText = '(kg)';
+        heightUnit.innerText = '(cm)';
+    } else {
+        weightUnit.innerText = '(lb)';
+        heightUnit.innerText = '(inches)';
+    }
 }
